e73c2d3fd5e11b5f55a64ca96f6917196c149594
sadowbass <shinwa46@naver.com>
2023-10-09 19:39:35 +0900

    change get ipaddress

--- src/main/java/com/example/jenkinstest/ApiController.java
+++ src/main/java/com/example/jenkinstest/ApiController.java
@@ -1,5 +1,6 @@
 package com.example.jenkinstest;
 
+import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -9,12 +10,18 @@ import java.net.UnknownHostException;
 @RestController
 public class ApiController {
 
+    private final Environment environment;
+
+    public ApiController(Environment environment) {
+        this.environment = environment;
+    }
+
     @GetMapping("/")
     public String api() throws UnknownHostException {
         InetAddress inetAddress = InetAddress.getLocalHost();
         System.out.println("inetAddress.getHostAddress() = " + inetAddress.getHostAddress());
         System.out.println("inetAddress.getCanonicalHostName() = " + inetAddress.getCanonicalHostName());
         System.out.println("inetAddress.getHostName() = " + inetAddress.getHostName());
-        return inetAddress.getHostAddress();
+        return environment.getProperty("server.ip");
     }
 }
